9a86815456b2f9b50e67ce58a4dc561e054e9f04
0xkf <Kanichi73@me.com>
2023-10-10 02:14:41 +0900

    test scipts...

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,11 +4,15 @@ const fs = require("fs");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
+  console.log("deployer is " + await deployer.address)
   const balance = await deployer.getBalance();
+  console.log("balance is " + await balance)
   const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  console.log("deploying marketplace contract ")
   const marketplace = await Marketplace.deploy();
 
   await marketplace.deployed();
+  console.log("deploying")
 
   const data = {
     address: marketplace.address,
@@ -17,6 +21,8 @@ async function main() {
 
   //This writes the ABI and address to the mktplace.json
   fs.writeFileSync('./src/Marketplace.json', JSON.stringify(data))
+  console.log("address is " + await data.address);
+  console.log("abi is generating");
 }
 
 main()
